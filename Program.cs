1ec5f87363162d9909e15f6a97f6ce29e917114c
Lavanya Chilakalapudi <lavanya13@usf.edu>
2024-02-28 11:28:17 -0500

    Changed the commits

--- Program.cs
+++ Program.cs
@@ -3,22 +3,19 @@
 int[] bubble_sort_numbers = { 5, 72, 3, 14, 7, 1 };
 Console.WriteLine("");
 
-Console.WriteLine("Here is the original array that will be used to execute the Bubble Sort Algorithm: ");
+Console.WriteLine("original array");
 for (int i = 0; i < bubble_sort_numbers.Length; i++)
 {
     Console.Write("  " + bubble_sort_numbers[i]);
 }
 
 Console.WriteLine("");
-Console.WriteLine("Here is the array after the execution of the Bubble Sort Algorithm: ");
+Console.WriteLine("Array after bubble sort");
 Bubble_Sort(bubble_sort_numbers);
 for (int i = 0; i < bubble_sort_numbers.Length; i++)
 {
     Console.Write("  " + bubble_sort_numbers[i]);
 }
-Console.WriteLine("");
-Console.WriteLine("Press any key to exit the program ...");
-Console.ReadKey(true);
 
 static void Bubble_Sort(int[] arr)
 {
